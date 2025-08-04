06051d581b4ea82ecbd91668fe4e6937f70d8ef3
Breno Soriani <breno.soriani@gmail.com>
2025-08-04 15:26:13 -0300

    Alterando as configurações

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,26 +1,22 @@
-import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-
-const secret = process.env.JWT_SECRET || "sua_chave_super_segura";
-
-interface JwtPayload {
-  id: string;
-}
+import { Request, Response, NextFunction } from "express";
 
 export function authenticate(req: Request, res: Response, next: NextFunction) {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!authHeader) {
     return res.status(401).json({ message: "Token não fornecido." });
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, secret) as JwtPayload;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+      id: string;
+    };
     req.usuarioId = decoded.id;
     next();
-  } catch (error) {
-    return res.status(401).json({ message: "Token inválido ou expirado." });
+  } catch {
+    return res.status(401).json({ message: "Token inválido" });
   }
 }
